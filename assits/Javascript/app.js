29c7929d1cd3d494118f2999248bdb39770a7655
bhelms2132 <bhelms2132@gmail.com>
2019-08-13 18:10:37 -0400

    div id created for quiz and results, button id created for submit in html

--- assits/Javascript/app.js
+++ assits/Javascript/app.js
@@ -1,8 +1,34 @@
 //--Time allowed--//
 var number = 100;
 
+
+
 //--Timer interval--//
 var intervalId ;
 
+
+
 //--Start function--//
-$("#start").on("click",start);
+run();
+
+
+
+//-- --//
+function run() {
+    clearInterval(intervalId);
+    intervalId = setInterval(decrement, 1000);
+}
+
+//--Decrement function--//
+function decrement() {
+      
+    number--;
+//--Show the number in the #show-number tag.--//
+    $("#show-number").html("<h2>" + number + "</h2>");
+
+    if (number === 0) {
+        stop();
+        alert("Time Up!");
+    }
+}
+
