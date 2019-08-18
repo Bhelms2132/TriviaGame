73404fd0074dbfc2d006e659d2bc60f251b02ac6
bhelms2132 <bhelms2132@gmail.com>
2019-08-18 15:35:15 -0400

    QUestions and Answers working

--- assits/Javascript/app.js
+++ assits/Javascript/app.js
@@ -1,146 +1,175 @@
-
-//--Show function--//
-$(document).ready(function(){
-    $("#start").click(function(){
-        $("#show-number").start();
-    });
-
-});
-
-//-- Timer--//
+//--Declaring var needed--//
 var number = 100;
+var currentQuestion = 0;
+var score = 0;
+var lost = 0;
 var intervalId;
 
 
 
-function run() {
-    clearInterval(intervalId);
+  function run() {
     intervalId = setInterval(decrement, 1000);
-}
+  }
 
-//--Decrement fuction--//
-function decrement(){
+  function decrement() {
 
-    //--Decrease number by one--//
     number--;
 
-    $("#show-number").html("<h2>" + number + "<h2>");
+    $("#show-number").html("<h2>" + number + "</h2>");
 
     if (number === 0) {
 
-        stop();
+      stop();
     }
-}
+  }
 
-function stop(){
+  function stop() {
 
     clearInterval(intervalId);
-}
+    alert("Times Up!")
+  }
+  run();
 
-run();
-
-console.log(number);
-
-
-
-
-
-
-
-
-
-
-    
-
-
-
-
-
-
-var myQuestions = [
+//--Questions/Answers array--//
+const myQuestions = [
     {
         question: "In the 1983 Thiller 'Cujo', this sweet fluffy St.Bernard was bitten by what animal before terrorizing a small town and a stay at home mom and her son ?",
-     
-        answers: {
-            a: 'Squirrle',
-            b: 'The Geico Lizard',
-            c: 'bat',
-        },
-        correctAnswer: 'bat'
+        choices: ["Snake", "Coyote", "Bat"],
+        correctAnswer: "Bat"
     },
 
     {
-        question: "In the 1984 Fantasy 'Ghost Busters', Ray tried to think of the most harmless thing, something he loved from his childhood that the Ghost Busters wouldul have to fight. What was it?",
-         
-        answers: {
-            a: 'Barney, the big purple dinasor',
-            b: 'Stay Puft MarshMellow Man',
-            c: 'Puff the Magic Dragon',
-         },
-         correctAnswer:'Stay Puft MarshMellow Man'
+         question: "In the 1984 Fantasy 'Ghost Busters', Ray tried to think of the most harmless thing, something he loved from his childhood that the Ghost Busters wouldul have to fight. What was it?",
+         choices: ["Barney, the big purple dinasor", "Stay Puft Marshmellow Man", "His dog"],
+         correctAnswer:"Stay Puft MarshMellow Man"
 
     },
 
     {
         question: "In the 1983 Drama 'Risky Buisness', what actor played the charactor 'Joel'?",
-        
-        answers: {
-            a:'David Hasselhoff',
-            b:'Tom Cruise',
-            c:'Woody Harrelson',
-
-         },
-         correctAnswer: 'Tom Cruise'
+        choices: ["David Hasselhoff", "Tom Cruise", "Michael J. Fox"],
+        correctAnswer: "Tom Cruise"
     },
 
     {
         question: "In the 1988 Drama 'Big', what did the 12 year old boy wish for?",
-
-        answers: {
-            a:'Car',
-            b:'Girlfriend',
-            c:'To be an Adult',
-        },
-        correctAnswer:'To be an Adult'
+        choices: ["A drivers license", "Girlfriend", "To be an adult"],
+        correctAnswer:"To be an Adult"
     },
 
     {
         question: "In the 1985 Fantasy/Cult 'Teen Wolf', Scott Howard learns that he can turn into what animal?",
-
-        answers: {
-            a:'WereWolf',
-            b:'Marmot',
-            c:'Elephant',
-        },
-        correctAnswer:'WereWolf'
-
+        choices: ["WereWolf", "Marmot", "Cat"],
+        correctAnswer:"WereWolf"
     },
     
     {
         question: "In the 1985 Fantasy/Scfi 'Back To The Future', Marty Mcfly drove what car back in time?",
-
-        answers: {
-            a:'El Comino',
-            b:'F150',
-            c:'Delorean',
-        },
-        correctAnswer:'Delorean'
-
+        choices: ["El Comino", "Corvette", "Delorean"],
+        correctAnswer:"Delorean"
     },
 
     {
         question: "In the 1981 Fantasy/Action 'Raiders of the LOst Ark', what actor played Indian Jones charactor?",
-
-        answers:{
-            a:'Harrison Ford',
-            b:'Clint Eastwood',
-            c:'Burt Reynolds',
-        },
+        choices:["Harrison Ford", "Clint Eastwood", "Burt Reynolds"],
         correctAnswer:'Harrison Ford',
     },
-    console.log(myQuestions)
+    
 ];
- console.log(myQuestions);
+
+console.log(myQuestions);
+
+ 
+function nextQuestion(){
+
+    const isQuestionOver = (myQuestions.length -1) === currentQuestion;
+
+    if (isQuestionOver){
+        console.log("Game Over!");
+        displayResult();
+    }else{
+        currentQuestion++;
+        loadQuestion();
+    }
+    
+}
+
+
+function loadQuestion(){
+
+    const question = myQuestions[currentQuestion].question;
+    const choices = myQuestions[currentQuestion].choices;
+    
+   
+    $("#game").html(`
+        <h4>${question}<h4>  
+       ${loadChoices(choices)}`);
+}
+
+function loadChoices(choices){
+    var result = "";
+    for(var i = 0; i < choices.length; i++){
+        result += `<p class="choice" data-answer="${choices[i]}">${choices[i]}</p>`;
+    }
+    return result;
+}
+
+$(document).on("click", ".choice", function(){
+    const selectedAnswer = $(this).attr("data-answer");
+    const correctAnswer = myQuestions[currentQuestion].correctAnswer;
+
+    if (correctAnswer === selectedAnswer){
+
+        score++;
+        console.log("Correct!");
+        nextQuestion();
+        
+    }else{
+       
+        lost++;
+        console.log("Wrong!");
+        nextQuestion();
+    }
+    
+});
+
+function displayResult(){
+    const result = `
+     <p>You got ${score} Correct questions</p>
+     <p>You got ${lost}  Wrong questions</p>
+     <p>Total ${myQuestions.length} questions</p>
+     
+     `;
+    $("#game").html(result);
+}
+
+
+
+$("#start").click(function(){
+   $("#start").remove();
+   $("#show-number").html(number);
+   loadQuestion();
+});
+  
+   
+   
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
 
 
 
